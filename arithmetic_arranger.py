6c95409239b9b72ee40a568df031252ef6e3b702
lsr <lsrprntr@gmail.com>
2023-05-05 10:49:14 +1000

    complete draft

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,23 +1,37 @@
 def arithmetic_arranger(problems,req=False):#list of problems given, requests for the answer default False
-    if len(problems)>5:raise "Error: Too many problems."
-    tops,bots,totals=list()
-    print(tops,bots,totals)
-    for i in problems:
-        top,sign,bot = i.split()
-        if sign not in "+-":
-            raise "Error: Operator must be '+' or '-'."
+    if len(problems)>5:return print("Error: Too many problems.") #Error 1
+
+    tops,bots,totals,dashes=[],[],[],[]
+    
+    for problem in problems:
+
+        top,sign,bot = problem.split()
+
+        if sign not in "+-": #Error 2
+            return print("Error: Operator must be '+' or '-'.") 
         try:
             if sign == "+":
                 total = int(top)+int(bot)
             else:
                 total = int(top)-int(bot)
         except:
-            raise "Error: Numbers must only contain digits."
-        #format to lists
+            return print("Error: Numbers must only contain digits.") #Error 3
+        if len(top)>4 or len(bot)>4: return print("Error: Numbers cannot be more than four digits.") #Error 4
+
+        #format spacing
+        space = max(len(str(top)),len(str(bot)))+2
+        top = str(top).rjust(space)
+        bot = sign+str(bot).rjust(space-1)
+        total = str(total).rjust(space)
+        dash = "-"*space
+        #add to lists
         tops.append(top)
         bots.append(bot)
         totals.append(total)
-        
-    return "AAA"
+        dashes.append(dash)
+    if req == True:
+        return print(f"{'    '.join(tops)}\n{'    '.join(bots)}\n{'    '.join(dashes)}\n{'    '.join(totals)}")
+    else:
+        return print(f"{'    '.join(tops)}\n{'    '.join(bots)}\n{'    '.join(dashes)}")
 
-arithmetic_arranger(["32 / 698", "3801 - 2", "45 + 43", "123 + 49"],True)
+arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"],True)
