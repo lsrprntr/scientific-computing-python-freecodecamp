543c74aa807a8fd4ed012be317f9f5819db8912b
lsr <lsrprntr@gmail.com>
2023-05-05 13:03:58 +1000

    coding

--- time_calculator.py
+++ time_calculator.py
@@ -5,6 +5,7 @@ def add_time(start, duration,days=None):
     time,MM = start.split()
     hour,min = time.split(":")
     addhour,addmin = duration.split(":")
+    dayadd=0
 
     #mins
     newmin = int(min)+int(addmin)
@@ -16,27 +17,33 @@ def add_time(start, duration,days=None):
     
     #hours
     newhour = int(hour)+int(addhour)
-    if newhour >= (12-int(hour)): #AM/PM switch calc
+    if newhour >= (12-int(hour)): #AM/PM switch and days elapsed calc
+        dayadd = (newhour + 12 if MM == "PM" else 0)//24
+
         switch = newhour//12%2
+
         if switch == 1:
             if MM == "AM":
                 MM == "PM"
             else:
                 MM = "AM"
-       
+        
+    
     newhour = (newhour)%12
+    if newhour == 0:
+        newhour = "12"
     
     
 
     #AM/PM calc
-    if int(addhour)//12%2==0:
-        days =1
+    #if int(addhour)//12%2==0:
+    #    days =1
     
     #format
     print("Start time", start,"Add",duration)
     print("Newtime", newhour,":",newmin,MM)
-    print("Day result", days)
-    print("Days elapsed",days)
+    #print("Day result", days)
+    print("Days elapsed",dayadd)
     return
 
-add_time("1:01 PM","23:59")
+add_time("1:00 PM","48:00")
