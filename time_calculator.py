c73c87f7e9b5a27c77e7aae026ae9fbf29736b72
lsr <lsrprntr@gmail.com>
2023-05-05 12:02:30 +1000

    coding

--- time_calculator.py
+++ time_calculator.py
@@ -3,14 +3,22 @@
 
 def add_time(start, duration,day=None):
     hours = [1,2,3,4,5,6,7,8,9,10,11,12]
-    hour,min = start.split(":")
-    min,MM = min.split()
+    time,MM = start.split()
+    hour,min = time.split(":")
+    
     addhour,addmin = duration.split(":")
 
+    newhour = (int(hour)+int(addhour))%12
+    newmin = (int(min)+int(addmin))%60
 
-    print(hours[16%12])
+    print(newhour)
+
+    print(newmin)
+
+    if int(addhour)//12%2==0:
+        print("switch ampm")
     
     
     return
 
-add_time("10:20 PM","23:22")
+add_time("2:20 PM","2:40")
