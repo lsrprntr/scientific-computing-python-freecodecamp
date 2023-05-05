77f229caba944e366a69547f162a4e2e6c0fb59f
lsr <lsrprntr@gmail.com>
2023-05-05 11:42:57 +1000

    drafting

--- time_calculator.py
+++ time_calculator.py
@@ -1,9 +1,16 @@
 #https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/time-calculator
+#No imports allowed for this exercise
 
-def add_time(start, duration):
+def add_time(start, duration,day=None):
+    hours = [1,2,3,4,5,6,7,8,9,10,11,12]
+    start.split(":")
+    
+    print(hours[16%12])
+    
+    
 
 
 
+    return
 
-
-    return new_time
+add_time("10:20 PM","23:22")
