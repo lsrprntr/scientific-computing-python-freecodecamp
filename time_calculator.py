552aeeaa272f040825b2ccffd1a7a5700b35306e
lsr <lsrprntr@gmail.com>
2023-05-05 11:34:04 +1000

    draft

---
+++ time_calculator.py
@@ -0,0 +1,9 @@
+#https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/time-calculator
+
+def add_time(start, duration):
+
+
+
+
+
+    return new_time
