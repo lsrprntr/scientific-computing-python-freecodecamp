1385b61af66c74ab09ba5776b09016bd96de0591
lsr <lsrprntr@gmail.com>
2023-05-05 16:09:39 +1000

    bug fixing

--- time_calculator.py
+++ time_calculator.py
@@ -14,36 +14,42 @@ def add_time(start, duration,days=None):
     newmin = (newmin)%60
     if newmin == 0:
         newmin = "00"
+    elif newmin < 10:
+        newmin = "0"+str(newmin)
     
     #hours
     newhour = int(hour)+int(addhour)
     if newhour >= (12-int(hour)): #AM/PM switch and days elapsed calc
-        dayadd = (newhour + 12 if MM == "PM" else 0)//24
-
+        dayadd = (newhour + (12 if MM == "PM" else 0))//24
         switch = newhour//12%2
-
         if switch == 1:
             if MM == "AM":
                 MM == "PM"
             else:
                 MM = "AM"
-        
-    
     newhour = (newhour)%12
     if newhour == 0:
         newhour = "12"
+
+    #days of week
+    if days != None:
+        week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+        day = week[(week.index(days.title())+dayadd)%7]
     
+    if dayadd == 1:
+        dayadd = "(next day)"
+    elif dayadd >=1:
+        dayadd = f"({dayadd} days later)"
+
+    if days != None:
+        if dayadd == 0:
+            return print(f"{newhour}:{newmin} {MM}, {day}")
+        return print(f"{newhour}:{newmin} {MM}, {day} {dayadd}")
+
+    if dayadd == 0:
+        return print(f"{newhour}:{newmin} {MM}")
+    return print(f"{newhour}:{newmin} {MM} {dayadd}")
+
     
 
-    #AM/PM calc
-    #if int(addhour)//12%2==0:
-    #    days =1
-    
-    #format
-    print("Start time", start,"Add",duration)
-    print("Newtime", newhour,":",newmin,MM)
-    #print("Day result", days)
-    print("Days elapsed",dayadd)
-    return
-
-add_time("1:00 PM","48:00")
+add_time("11:55 AM","3:12","saturday")
