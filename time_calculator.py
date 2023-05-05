f2438ba42450ff8dad3c00b93e3a84378954dc3d
lsr <lsrprntr@gmail.com>
2023-05-05 12:37:27 +1000

    coding

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,7 @@
 #https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/time-calculator
 #No imports allowed for this exercise
 
-def add_time(start, duration,day=None):
+def add_time(start, duration,days=None):
     time,MM = start.split()
     hour,min = time.split(":")
     addhour,addmin = duration.split(":")
@@ -11,22 +11,30 @@ def add_time(start, duration,day=None):
     if newmin >=60:
         hour = int(hour)+1
     newmin = (newmin)%60
+    if newmin == 0:
+        newmin = "00"
     
     #hours
     newhour = int(hour)+int(addhour)
-    if newhour >=12:
-        days = 1
+    if newhour >= (12-int(hour)): #AM/PM switch calc
+        print("Greater than 12 hours")
+        
+        if MM == "AM":
+            MM = "PM"
+        
     newhour = (newhour)%12
     
-    print(newhour)
-    print(newmin)
+    
 
     #AM/PM calc
     if int(addhour)//12%2==0:
-        print("switch ampm")
+        days =1
     
     #format
-
+    print("Start time", start,"Add",duration)
+    print("Newtime", newhour,":",newmin,MM)
+    print("Day result", days)
+    print("Days elapsed",days)
     return
 
-add_time("12:20 PM","2:40")
+add_time("1:01 PM","11:59")
