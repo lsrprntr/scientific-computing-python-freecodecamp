198d13c5804cc1f4c4f6d0dcc52ec96ca3faed57
lsr <lsrprntr@gmail.com>
2023-05-17 08:45:57 +1000

    coding after moving

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,48 +1,64 @@
 import copy
 import random
+from collections import Counter
 
 class Hat:
     def __init__(self,**kwargs):
-        self.contents,self.total = [],0
+        self.contents = []
+        self.total = 0
+        self.drawn = []
+
         for key,value in kwargs.items():
             self.total += value
             for i in range(1,value+1):
                 self.contents.append(key)
-        self.original = list(self.contents)
-        self.oritotal = int(self.total)
+
+        self.original = list(self.contents) #copy
+        self.oritotal = int(self.total) #copy
 
     def draw(self):
         if self.total == 0: #returns to original bag contents
-            self.contents = list(self.original)
+            self.contents = list(self.original) #copy
             self.total = self.oritotal
-            return print("Restart")
+            self.drawn = []
+            return print("Restarted/Set to original")
         else:
             r = random.randint(0,self.total-1)
             ball = self.contents[r]
             self.contents.pop(r)
             self.total-=1
-            return ball
+            self.drawn.append(ball)
+            return self.drawn
         
 
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    d = hat.draw()
+    m = num_balls_drawn
+    n = int(num_experiments) #copy
+    success = 0
+
+    while num_experiments > 0:
+        num_experiments -= 1
+        problist = list()
+        drawpile = list()
+        for i in range(0,m):
+            hat.draw()
+        print(hat.drawn)
+        d = Counter(hat.drawn)
+        for key in expected_balls:
+            if d[key] >= expected_balls[key]:
+                print("Matches prob")
+            else:
+                print("No Match")
+                break
+            success += 1
+                
+            
+
+
     
-    return
+    probability = success/n
+    return probability
 
 hat1 = Hat(yellow=1,dfdas=1)
-hat1.draw()
-hat1.draw()
-
-hat1.draw()
-hat1.draw()
-
-hat1.draw()
-hat1.draw()
-
-hat1.draw()
-hat1.draw()
-
-hat1.draw()
-hat1.draw()
